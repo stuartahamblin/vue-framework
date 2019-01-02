f18c938ab9b26342b7956dfbb8779500872030b6
stuartahamblin <stuart.a.hamblin@gmail.com>
2019-01-02 14:56:23 -0600

    update npm

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-user></app-user>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
